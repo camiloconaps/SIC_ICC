a54d4bf94f83d21046218c5bb869a563e6cdd96e
camiloconaps <camilososped@gmail.com>
2017-02-16 16:08:05 -0500

    Delete srctodb.php

--- source/srctodb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conex=mysql_connect('190.24.138.149','codensa','c0d3ns4') or die('no hay conexion');
-mysql_select_db('chat_atenea',$conex);
-
-//determinar la hora de conexion
-$fecha_actual = date('Y-m-d H:i:s');
-$hora_actual = date('H:i:s');
-?>
-		
